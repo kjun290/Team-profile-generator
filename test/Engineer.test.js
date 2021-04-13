49e870b10026d405dbe6fa8f735d7787e0b8d79b
Kevin Jun <kwjun90@gmail.com>
2021-04-12 23:31:01 -0500

    completed engineer test

--- test/Engineer.test.js
+++ test/Engineer.test.js
@@ -1,1 +1,23 @@
-const Employee = require("../lib/Employee")
+const { expect } = require("@jest/globals");
+const Employee = require("../lib/Employee");
+const Engineer = require("../lib/Engineer");
+
+    test("set github using const", () => {
+        const gitHub = "kjun290";
+        const e = new Engineer ("Kevin", 123, "kevin@email.com", gitHub);
+        expect(e.github).toBe(gitHub);
+
+    });
+
+    test("getRole() returns Engineer", () => {
+        const role = "Engineer";
+        const e = new Engineer("Kevin", 123, "kevin@email.com","kjun290");
+        expect(e.getRole()).toBe(role);
+    })
+
+    test("getGithub() should return gitHub", () => {
+        const gitHub = "kjun290";
+        const e = new Engineer("Kevin", 123, "kevin@email.com", gitHub);
+        expect(e.getGithub()).toBe(gitHub);
+    })
+        
--- test/Manager.test.js
+++ test/Manager.test.js
@@ -8,16 +8,16 @@ const Employee = require("../lib/Employee");
         const officenumber = 100;
         const e = new Manager ("Kevin", 123, "kevin@email.com", officenumber);
         expect(e.officeNumber).toBe(officenumber);
-    }) 
+    }); 
 
     test ("getRole() returns Manager", () => {
         const role = "Manager";
         const e = new Manager("Kevin", 123, "kevin@email.com", 100);
         expect(e.getRole()).toBe(role);
-    })
+    });
 
     test ("getOfficeNumber() returns officenumber", () => {
         const officenumber = 100;
-        const e = new Manager ("kevin", 123, "kevin@email.com", 100);
+        const e = new Manager ("kevin", 123, "kevin@email.com", officenumber);
         expect(e.getofficeNumber()).toBe(officenumber);
     })
